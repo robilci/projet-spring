ec3ed561f3805932ff92d8f591eea08b8b4dbb99
= <=>
2020-12-03 13:51:03 +0100

    création entité model de demande

---
+++ src/main/java/spring/core/entity/Model.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spring.core.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author robin
+ */
+@Entity
+@Table(uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"title"})})
+public class Model implements Serializable {
+
+    @Id
+    private Long id;
+    
+    @NotNull
+    private String title;
+    
+    @NotNull
+    private int priority;
+    
+    @NotNull
+    private int type;
+    
+    @NotNull
+    private String localisation;
+
+    public Model(String title, int priority, int type, String localisation) {
+        this.title = title;
+        this.priority = priority;
+        this.type = type;
+        this.localisation = localisation;
+    }
+  
+    public Model(){}
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+      
+}
