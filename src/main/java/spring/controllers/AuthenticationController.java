43da6a6b484a7a9147d7cd00e6d1553205d669ea
= <=>
2021-01-13 12:51:04 +0100

    erreur auth

--- src/main/java/spring/controllers/AuthenticationController.java
+++ src/main/java/spring/controllers/AuthenticationController.java
@@ -44,14 +44,7 @@ public class AuthenticationController {
 
     @GetMapping("/")
     public String loginForm(Model model, HttpSession session) {
-        if (session != null) {
-            if (session.getAttribute("role") != null) {
-                return this.modelController.list(model);
-            }
-        }
-        model.addAttribute("login", new Login());
-
-        return "login";
+        return loginForm2(model, session);
     }
 
     @GetMapping("/login")
@@ -71,19 +64,21 @@ public class AuthenticationController {
         session.invalidate();
         model.addAttribute("logout", "Vous avez été déconnecté avec succès");
         model.addAttribute("login", new Login());
-        return "login";
+        
+        return "redirect:http://localhost:8080/login";
     }
 
     @PostMapping("/login")
     public String loginSubmit(@ModelAttribute Login login, Model model, HttpSession session) {
-
+        
         User user = userService.findByPasswordAndEmail(login.getPassword(), login.getEmail());
 
         if (user == null) {
-            model.addAttribute("error", "Email ou mot de passe incorrect");
+            model.addAttribute("error", "1");
             session.invalidate();
             return "login";
         } else {
+            model.addAttribute("error", "0");
             session.setAttribute("firstname", user.getFirstname());
             session.setAttribute("lastname", user.getLastname());
             session.setAttribute("role", user.getRole().getName());
